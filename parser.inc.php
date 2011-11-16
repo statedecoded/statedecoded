3c4b79b54c53ff1fa0dfc6d1b5fb15a2f5d094f6
Waldo Jaquith <waldo@jaquith.org>
2011-11-16 18:37:12 -0500

    Deleting unnecessary PHP debug verbosity.

--- parser.inc.php
+++ parser.inc.php
@@ -1,10 +1,5 @@
 <?php
 
-# During this debug phase, I want to know about errors.
-ini_set('display_errors', 1);
-ini_set('error_reporting', 'E_ALL');
-ini_set('error_log','errors.log');
-
 class Parser
 {
 	
