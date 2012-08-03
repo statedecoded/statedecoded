d6f02c7260fff0da9e233578cf52910f73401b00
Waldo Jaquith <waldo@jaquith.org>
2012-08-03 10:33:04 -0400

    Fixing buggy APC constant caching code
    Switching to the use of the more-reliable ini_get() to determine
    whether APC is enabled and improving the constant loading code to
    actually save labor, rather than uselessly loading constants and saving
    them to APC with every page view.

--- includes/page-head.inc.php
+++ includes/page-head.inc.php
@@ -3,22 +3,36 @@
 # Include the PEAR database abstraction layer. <http://pear.php.net/package/MDB2>
 require_once 'MDB2.php';
 
-# Include the site's config file.
-require_once 'config.inc.php';
-
-# If APC is installed on this server, load some assets into APC.
-if (GID_EXTENSION_LOADED_APC === true)
+# If APC is running.
+if (ini_get('apc.enabled') == 1)
 {
-	# Generate a listing of all of the constants that were defined in config.inc.php.
-	$constants = get_defined_constants(true);
-	apc_define_constants('settings', $constants['user']);
-	apc_load_constants('settings');
+	# Attempt to load the config file constants out of APC.
+	$result = apc_load_constants('config');
+	
+	# If this attempt did not work.
+	if ($result === false)
+	{
+		# Load constants from the config file.
+		require_once 'config.inc.php';
+		
+		# And then save them to APC.
+		$constants = get_defined_constants(true);
+		apc_define_constants('config', $constants['user']);
+	}
+}
+
+# Else if APC is not installed.
+else
+{
+	# Include the site's config file.
+	require_once 'config.inc.php';
 }
 
 # Connect to the database.
 $db =& MDB2::connect(MYSQL_DSN);
 if (PEAR::isError($db))
 {
+	mail('waldo@jaquith.org', 'VA Laws DB Connection Failed', __FILE__."\r\r\r".$db->getMessage());
 	die('We’re having some database trouble right now. Check back later. Sorry!');
 }
 
@@ -27,6 +41,9 @@ $db->setCharset('utf8');
 
 # Include the functions that drive the site.
 require_once('functions.inc.php');
+
+# Include Solarium's header, for queries to Solr.
+require_once('solarium/Autoloader.php');
 
 # If there exists a custom functions file, include that, too.
 if (defined('CUSTOM_FUNCTIONS'))
