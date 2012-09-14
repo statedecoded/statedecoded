fe206f9b8448a4fe10c7393ba9a4c5132f60b036
Waldo Jaquith <waldo@jaquith.org>
2012-09-14 12:56:37 -0400

    De-Virginia-ing, removing tag code
    Correcting a mistake made with v0.3—the accidental inclusion of
    Virginia-specific text. Also, removing the code that displays the tag
    clou, since there's currently no method for adding tags (either in bulk
    or one-off).

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,7 +6,7 @@ require $_SERVER['DOCUMENT_ROOT'].'/../includes/page-head.inc.php';
 # Fire up our templating engine.
 $template = new Page;
 
-$template->field->browser_title = 'Virginia Decoded: The Code of Virginia, for Humans.';
+$template->field->browser_title = SITE_TITLE.': The '.LAWS_NAME.', for Humans.';
 
 # Initialize our body variable.
 $body = '';
@@ -14,36 +14,9 @@ $body = '';
 # Initialize our sidebar variable.
 $sidebar = '
 	<section>
-	<h1>Welcome</h1>
-	<p>Virginia Decoded provides the Code of Virginia on one friendly website. Court decisions,
-	legislation past and present, inline definitions, a modern API, and all of the niceties of
-	modern website design. It’s like the expensive software lawyers use, but free and wonderful.</p>
-	
-	<p>This is a public beta test of Virginia Decoded, which is to say that everything is under
-	development. Things are funny looking, broken, and generally unreliable right now.</p>
-	
 	<p>Powered by <a href="http://www.statedecoded.com/">The State Decoded</a>.</p>
 	</section>';
 
-# Get a listing of tags and display them.
-$tags = new Tags;
-$tags->get();
-$tag_cloud = $tags->cloud();
-if (!empty($tag_cloud))
-{
-	$sidebar .= '
-		<section><h1>Topics</h1>
-			<div class="tag-cloud">'.$tag_cloud.'</div>
-		</section>';
-}
-
-# Show the most recent comments.
-$sidebar .= '
-	<section><h1>Recent Comments</h1>
-		<div id="recentcomments" class="dsq-widget">
-			<script src="http://vacode.disqus.com/recent_comments_widget.js?num_items=3&amp;hide_avatars=1&amp;avatar_size=32&amp;excerpt_length=200"></script>
-		</div>
-	</section>';
 
 # Get an object containing a listing of the titles in the code.
 $structure = new Structure();
@@ -51,10 +24,8 @@ $titles = $structure->list_children();
 
 $body .= '
 	<article>
-	<h1>Titles of the Code of Virginia</h1>
-	<p>These are the fundamental units of the Code of Virginia. There are 77 titles, roughly divided
-	up by topic. Each title is divided into chapters, 1,535 in all, and each chapter is divided
-	into sections (a.k.a. laws), 31,663 in all.</p>
+	<h1>Titles of the '.LAWS_NAME.'</h1>
+	<p>These are the fundamental units of the '.LAWS_NAME.'.</p>
 	<dl class="titles">';
 foreach ($titles as $title)
 {
