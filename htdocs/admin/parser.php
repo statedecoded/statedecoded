0dcb84e3ba1742ed6783c7bb473b98ca96327553
Waldo Jaquith <waldo@jaquith.org>
2012-12-21 16:27:55 -0500

    Made notification more descriptive

--- htdocs/admin/parser.php
+++ htdocs/admin/parser.php
@@ -71,12 +71,12 @@ elseif ($_POST['action'] == 'empty')
 	# Execute the query.
 	$result =& $db->exec($sql);
 	
-	echo '<p>Removed everything.</p>';
 	# Delete law histories.
 	$sql = 'DELETE FROM laws_meta
 			WHERE meta_key = "history"';
 	$result =& $db->exec($sql);
 	
+	echo '<p>Removed everything but titles from structure.</p>';
 	
 }
 
