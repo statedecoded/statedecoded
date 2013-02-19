8511ad6b0f3d6ccb29010be8c9a92e2f783e16ff
Waldo Jaquith <waldo@jaquith.org>
2013-02-19 15:13:50 -0500

    Add a docblock
    Per #155.

--- includes/page-head.inc.php
+++ includes/page-head.inc.php
@@ -1,9 +1,17 @@
 <?php
 
-
-
 /**
- * Loads the State Decoded environment.
+ * Loads the environment for The State Decoded.
+ * 
+ * PHP version 5
+ *
+ * @author		Waldo Jaquith <waldo at jaquith.org>
+ * @copyright	2010-2013 Waldo Jaquith
+ * @license		http://www.gnu.org/licenses/gpl.html GPL 3
+ * @version		0.6
+ * @link		http://www.statedecoded.com/
+ * @since		0.1
+ *
  */
 
 # Include the PEAR database abstraction layer. <http://pear.php.net/package/MDB2>
