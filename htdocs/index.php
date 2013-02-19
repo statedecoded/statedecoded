bbb29e85f109cb957e52cb0fa541d1bba612d95e
Waldo Jaquith <waldo@jaquith.org>
2013-02-19 15:24:48 -0500

    Update the comment style
    Per #170.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,5 @@
 <?php
 
-# Include the PHP declarations that drive this page.
 /**
  * The site home page.
  *
@@ -18,24 +17,35 @@
  *
  */
 
+/*
+ * Include the PHP declarations that drive this page.
+ */
 require $_SERVER['DOCUMENT_ROOT'].'/../includes/page-head.inc.php';
 
-# Fire up our templating engine.
+/*
+ * Fire up our templating engine.
+ */
 $template = new Page;
 
 $template->field->browser_title = SITE_TITLE.': The '.LAWS_NAME.', for Humans.';
 
-# Initialize our body variable.
+/*
+ * Initialize the body variable.
+ */
 $body = '';
 
-# Initialize our sidebar variable.
+/*
+ * Initialize the sidebar variable.
+ */
 $sidebar = '
 	<section>
 	<p>Powered by <a href="http://www.statedecoded.com/">The State Decoded</a>.</p>
 	</section>';
 
 
-# Get an object containing a listing of the titles in the code.
+/*
+ * Get an object containing a listing of the titles in the code.
+ */
 $structure = new Structure();
 $titles = $structure->list_children();
 
@@ -55,14 +65,20 @@ if ( !empty($titles) )
 }
 $body .= '</article>';
 
-# Put the shorthand $body variable into its proper place.
+/*
+ * Put the shorthand $body variable into its proper place.
+ */
 $template->field->body = $body;
 unset($body);
 
-# Put the shorthand $sidebar variable into its proper place.
+/*
+ * Put the shorthand $sidebar variable into its proper place.
+ */
 $template->field->sidebar = $sidebar;
 unset($sidebar);
 
-# Parse the template, which is a shortcut for a few steps that culminate in sending the content
-# to the browser.
+/*
+ * Parse the template, which is a shortcut for a few steps that culminate in sending the content
+ * to the browser.
+ */
 $template->parse();
