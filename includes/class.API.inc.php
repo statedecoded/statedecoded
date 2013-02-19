9053b689f4e792ab901243c6c691971adef4091c
Waldo Jaquith <waldo@jaquith.org>
2013-02-19 15:13:00 -0500

    Update the copyright year

--- includes/class.API.inc.php
+++ includes/class.API.inc.php
@@ -6,7 +6,7 @@
  * PHP version 5
  *
  * @author		Waldo Jaquith <waldo at jaquith.org>
- * @copyright	2010-2012 Waldo Jaquith
+ * @copyright	2010-2013 Waldo Jaquith
  * @license		http://www.gnu.org/licenses/gpl.html GPL 3
  * @version		0.6
  * @link		http://www.statedecoded.com/
