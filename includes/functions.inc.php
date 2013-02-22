5be0ffd03b02f091a8f1a8c020ed3c10536d5111
Waldo Jaquith <waldo@jaquith.org>
2013-02-22 10:44:40 -0500

    Update the comment style
    Per #170

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -39,10 +39,11 @@ function fetch_url($url)
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-	// Set CURLOPT_PROTOCOLS to protect against exploitation of CVE-2013-0249 that
-	// affects cURL 7.26.0 to and including 7.28.1.
-	// http://curl.haxx.se/docs/adv_20130206.html
-	// http://www.h-online.com/open/news/item/cURL-goes-wrong-1800880.html
+	/* Set CURLOPT_PROTOCOLS to protect against exploitation of CVE-2013-0249 that
+	 * affects cURL 7.26.0 to and including 7.28.1.
+	 * http://curl.haxx.se/docs/adv_20130206.html
+	 * http://www.h-online.com/open/news/item/cURL-goes-wrong-1800880.html
+	 */
 	$allowed_protocols = CURLPROTO_HTTP | CURLPROTO_HTTPS;
 	curl_setopt($ch, CURLOPT_PROTOCOLS, $allowed_protocols);
 	curl_setopt($ch, CURLOPT_REDIR_PROTOCOLS, $allowed_protocols & ~(CURLPROTO_FILE | CURLPROTO_SCP));
@@ -92,41 +93,54 @@ class Autolinker
 			return false;
 		}
 		
-		// If the provided term is an array of terms, just use the first one. This might seem odd,
-		// but note that this function is written to be used within preg_replace_callback(), the
-		// PCRE provides an array-based word listing, and we only want the first one.
-		if (is_array($term))
+		/*
+		 * If the provided term is an array of terms, just use the first one. This might seem odd,
+		 * but note that this function is written to be used within preg_replace_callback(), the
+		 * PCRE provides an array-based word listing, and we only want the first one.
+		 * (is_array($term))
+		 */
 		{
 			$term = $term[0];
 		}
 		
-		// If we have already marked this term as blacklisted -- that is, as a word that is a subset
-		// of a longer term -- then just return the term without marking it as a dictionary term.
+		/*
+		 * If we have already marked this term as blacklisted -- that is, as a word that is a subset
+		 * of a longer term -- then just return the term without marking it as a dictionary term.
+		 */
 		if (in_array(strtolower($term), $this->term_blacklist))
 		{
 			return $term;
 		}
 	
-		// Determine whether this term is made up of multiple words, so that we can eliminate any
-		// terms from our arrays of terms that are any of the individual words that make up this
-		// term. That is, if this term is "person or people," and "person" is another term in our
-		// array, then we want to drop "person," to avoid display overlapping terms.
+		/*
+		 * Determine whether this term is made up of multiple words, so that we can eliminate any
+		 * terms from our arrays of terms that are any of the individual words that make up this
+		 * term. That is, if this term is "person or people," and "person" is another term in our
+		 * array, then we want to drop "person," to avoid display overlapping terms.
+		 */
 		$num_spaces = substr_count($term, ' ');
+		
 		if ($num_spaces > 0)
 		{
 
-			// Use that separator to break the term up into an array of words.
+			/*
+			 * Use that separator to break the term up into an array of words.
+			 */
 			$term_components = explode(' ', $term);
 			
-			// Step through each the the words that make up this phrase, and add each of them to
-			// the blacklist, so that we can skip this word next time it appears in this law.
+			/*
+			 * Step through each the the words that make up this phrase, and add each of them to
+			 * the blacklist, so that we can skip this word next time it appears in this law.
+			 */
 			foreach ($term_components as $word)
 			{
 				$this->term_blacklist[] = strtolower($word);
 			}
 			
-			// Now step through each two-word sub-phrase that make up this 3+-word phrase (assuming
-			//that there are any) and add each of them to the blacklist.
+			/*
+			 * Now step through each two-word sub-phrase that make up this 3+-word phrase (assuming
+			 * that there are any) and add each of them to the blacklist.
+			 */
 			if ($num_spaces > 1)
 			{
 				for ($i=0; $i<$num_spaces; $i++)
@@ -145,24 +159,34 @@ class Autolinker
 	function replace_sections($matches)
 	{
 	
-		// PCRE provides an array-based match listing. We only want the first one.
+		/*
+		 * PCRE provides an array-based match listing. We only want the first one.
+		 */
 		$match = $matches[0];
 		
-		// If the section symbol prefixes this match, hack it off.
+		/*
+		 * If the section symbol prefixes this match, hack it off.
+		 */
 		if (substr($match, 0, strlen(SECTION_SYMBOL)) == SECTION_SYMBOL)
 		{
 			$match = substr($match, (strlen(SECTION_SYMBOL.' ')));
 		}
 	
-		// Create an instance of the Law class.
+		/*
+		 * Create an instance of the Law class.
+		 */
 		$law = new Law;
 		
-		// Just find out if this law exists.
+		/*
+		 * Just find out if this law exists.
+		 */
 		$law->section_number = $match;
 		$section = $law->exists();
 		
-		// If this isn't a valid section number, then just return the match verbatim -- there's no
-		// link to be provided.
+		/*
+		 * If this isn't a valid section number, then just return the match verbatim -- there's no
+		 * link to be provided.
+		 */
 		if ($section === false)
 		{
 			return $matches[0];
@@ -189,10 +213,15 @@ function json_error($text)
 	);
 	$error = json_encode($error);
 	
-	// Return a 400 "Bad Request" error. This indicates that the request was invalid. Whether this
-	// is the best HTTP header is unclear.
+	/*
+	 * Return a 400 "Bad Request" error. This indicates that the request was invalid. Whether this
+	 * is the best HTTP header is unclear.
+	 */
 	header("HTTP/1.0 400 OK");
-	// Send an HTTP header defining the content as JSON.
+	
+	/*
+	 * Send an HTTP header defining the content as JSON.
+	 */
 	header('Content-type: application/json');
 	echo $error;
 }
@@ -215,7 +244,7 @@ function sort_by_length($a, $b)
 }
 
 /**
- * 
+ * Turn the variables provided by each page into a rendered page.
  */
 class Page
 {
@@ -234,10 +263,14 @@ class Page
 	 */
 	function render()
 	{
-		// Save the contents of the template file to a variable.
+		/*
+		 * Save the contents of the template file to a variable.
+		 */
 		$this->html = file_get_contents(INCLUDE_PATH . '/templates/' . TEMPLATE . '.inc.php');
 		
-		// Create the browser title.
+		/*
+		 * Create the browser title.
+		 */
 		if (!isset($field->browser_title))
 		{
 			if (isset($field->page_title))
@@ -255,16 +288,22 @@ class Page
 			$field->browser_title .= '—'.SITE_TITLE;
 		}
 		
-		// Replace all of our in-page tokens with our defined variables.
+		/*
+		 * Replace all of our in-page tokens with our defined variables.
+		 */
 		foreach ($this->field as $field=>$contents)
 		{
 			$this->html = str_replace('{{'.$field.'}}', $contents, $this->html);
 		}
 		
-		// Erase any unpopulated tokens that remain in our template.
+		/*
+		 * Erase any unpopulated tokens that remain in our template.
+		 */
 		$this->html = preg_replace('/{{[0-9a-z_]+}}/', '', $this->html);
 		
-		// Erase selected containers, if they're empty.
+		/*
+		 * Erase selected containers, if they're empty.
+		 */
 		$this->html = preg_replace('/<section id="sidebar">(\s*)<\/section>/', '', $this->html);
 		$this->html = preg_replace('/<nav id="intercode">(\s*)<\/nav>/', '', $this->html);
 		$this->html = preg_replace('/<nav id="breadcrumbs">(\s*)<\/nav>/', '', $this->html);
@@ -297,17 +336,23 @@ class Dictionary
 	function define_term()
 	{
 
-		// We're going to need access to the database connection throughout this class.
+		/*
+		 * We're going to need access to the database connection throughout this class.
+		 */
 		global $db;
 		
-		// If no term has been defined, there is nothing to be done.
+		/*
+		 * If no term has been defined, there is nothing to be done.
+		 */
 		if (!isset($this->term))
 		{
 			return false;
 		}
 		
-		// Determine the structural heritage of the provided section number and store it in an
-		// array.
+		/*
+		 * Determine the structural heritage of the provided section number and store it in an
+		 * array.
+		 */
 		if (isset($this->section_number))
 		{
 			$heritage = new Law;
@@ -321,12 +366,17 @@ class Dictionary
 			}
 		}
 		
-		// We want to check if the term is in all caps. If it is, then we want to keep it in all
-		// caps to query the database. Otherwise, we lowercase it. That is, "Board" should be looked
-		// up as "board," but "NAIC" should be looked up as "NAIC."
+		/*
+		 * We want to check if the term is in all caps. If it is, then we want to keep it in
+		 * all caps to query the database. Otherwise, we lowercase it. That is, "Board" should be looked
+		 * up as "board," but "NAIC" should be looked up as "NAIC."
+		 */
 		for ($i=0; $i<strlen($this->term); $i++)
 		{
-			// If there are any uppercase characters, then make this PCRE string case sensitive.
+			/*
+			 * If there are any uppercase characters, then make this PCRE string case
+			 * sensitive.
+			 */
 			if ( (ord($this->term{$i}) >= 97) && (ord($this->term{$i}) <= 122) )
 			{
 				$lowercase = true;
@@ -339,8 +389,10 @@ class Dictionary
 			$this->term = strtolower($this->term);
 		}
 		
-		// If the last character in this word is an "s," then it might be a plural, in which case we
-		// need to search for this and without its plural version.
+		/*
+		 * If the last character in this word is an "s," then it might be a plural, in which
+		 * case we need to search for this and without its plural version.
+		 */
 		if (substr($this->term, -1) == 's')
 		{
 			$plural = true;
@@ -382,15 +434,18 @@ class Dictionary
 		
 			$sql .= 'LIMIT 1';
 		}
-
-		// Execute the query.
+		
 		$result =& $db->query($sql);
 
-		// If the query succeeds, great, retrieve it.
+		/*
+		 * If the query succeeds, great, retrieve it.
+		 */
 		if ( (PEAR::isError($result) === false) && ($result->numRows() > 0) )
 		{
 		
-			// Get all results.
+			/*
+			 * Get all results.
+			 */
 			$dictionary = new stdClass();
 			$i=0;
 			while ($term = $result->fetchRow(MDB2_FETCHMODE_OBJECT))
@@ -403,11 +458,15 @@ class Dictionary
 			}
 		}
 		
-		// Else if the query fails, then the term is found in the generic terms dictionary.
+		/*
+		 * Else if the query fails, then the term is found in the generic terms dictionary.
+		 */
 		else
 		{
 		
-			// Assemble the SQL.		
+			/*
+			 * Assemble the SQL.
+			 */
 			$sql = 'SELECT term, definition, source, source_url AS url
 					FROM dictionary_general
 					WHERE term="'.$db->escape($this->term).'"';
@@ -416,27 +475,30 @@ class Dictionary
 				$sql .= ' OR term = "'.$db->escape(substr($this->term, 0, -1)).'"';
 			}
 			$sql .= ' LIMIT 1';
-
-			// Execute the query.
+			
 			$result =& $db->query($sql);
 			
-			// If the query fails, or if no results are found, return false -- we have no terms for
-			// this chapter.
+			/*
+			 * If the query fails, or if no results are found, return false -- we have
+			 * no terms for this chapter.
+			 */
 			if ( (PEAR::isError($result) === true) || ($result->numRows() === 0) )
 			{
 				return false;
 			}
 		
-			// Get the first result. Assemble a slightly different response than for a custom term.
-			// We assign this to the first element of an object because that is the format that the
-			// API expects to receive a list of terms in. In this case, we have just one term.
+			/*
+			 * Get the first result. Assemble a slightly different response than for a
+			 * custom term. We assign this to the first element of an object because
+			 * that is the format that the API expects to receive a list of terms in. In
+			 * this case, we have just one term.
+			 */
 			$dictionary->{0} = $result->fetchRow(MDB2_FETCHMODE_OBJECT);
 			$dictionary->formatted = wptexturize($dictionary->definition).' (<a href="'.$dictionary->url.'">'
 				.$dictionary->source.'</a>)';
 
 		}
 		
-		// Return the result.
 		return $dictionary;
 		
 	}
@@ -448,18 +510,24 @@ class Dictionary
 	function term_list()
 	{
 
-		// We're going to need access to the database connection throughout this class.
+		/*
+		 * We're going to need access to the database connection throughout this class.
+		 */
 		global $db;
 		
-		// If a chapter ID hasn't been passed to this function, then return a listing of terms that
-		// apply to the entirety of the code.
+		/*
+		 * If a chapter ID hasn't been passed to this function, then return a listing of terms that
+		 * apply to the entirety of the code.
+		 */
 		if (!isset($this->structure_id) && !isset($this->scope))
 		{
 			$this->scope = 'global';
 		}
 		
-		// Get a listing of all structural units that contain the current structural unit -- that is,
-		// if this is a chapter, get the ID of both the chapter and the title. And so on.
+		/*
+		 * Get a listing of all structural units that contain the current structural unit -- that is,
+		 * if this is a chapter, get the ID of both the chapter and the title. And so on.
+		 */
 		if (isset($this->structure_id))
 		{
 			$heritage = new Structure;
@@ -474,7 +542,9 @@ class Dictionary
 			unset($tmp);
 		}
 
-		// Get a listing of all globally scoped terms.
+		/*
+		 * Get a listing of all globally scoped terms.
+		 */
 		if ($this->scope == 'global')
 		{
 			$sql = 'SELECT dictionary.term
@@ -484,9 +554,11 @@ class Dictionary
 					 WHERE scope="global"';
 		}
 		
-		// Otherwise, we're getting a listing of all more narrowly scoped terms. We always make sure
-		// that global definitions are included, in addition to the definitions for the current
-		// structural heritage.
+		/*
+		 * Otherwise, we're getting a listing of all more narrowly scoped terms. We always make sure
+		 * that global definitions are included, in addition to the definitions for the current
+		 * structural heritage.
+		 */
 		else
 		{
 			$sql = 'SELECT DISTINCT dictionary.term
@@ -508,17 +580,20 @@ class Dictionary
 			$sql .= ' OR (scope="global")';
 		}
 
-		// Execute the query.
 		$result =& $db->query($sql);
 		
-		// If the query fails, or if no results are found, return false -- we have no terms for this
-		// chapter.
+		/*
+		 * If the query fails, or if no results are found, return false -- we have no terms for this
+		 * chapter.
+		 */
 		if ( PEAR::isError($result) || ($result->numRows() < 1) )
 		{
 			return false;
 		}
 		
-		// Build up the result as an object as we loop through the results.
+		/*
+		 * Build up the result as an object as we loop through the results.
+		 */
 		$i=0;
 		while ($term = $result->fetchRow(MDB2_FETCHMODE_OBJECT))
 		{
@@ -526,19 +601,24 @@ class Dictionary
 			$i++;
 		}
 
-		// Assemble a second query, this one against our generic legal dictionary.
+		/*
+		 * Assemble a second query, this one against our generic legal dictionary.
+		 */
 		$sql = 'SELECT term
 				FROM dictionary_general';
 
-		// Execute the query.
 		$result =& $db->query($sql);
 		
-		// If the query fails, or if no results are found, return false -- we have no terms for this
-		// chapter.
+		/*
+		 * If the query fails, or if no results are found, return false -- we have no terms for this
+		 * chapter.
+		 */
 		if ($result->numRows() >= 1)
 		{		
-			// Append these results to the existing $terms object, continuing to use the previously-
-			// defined $i counter.
+			/*
+			* Append these results to the existing $terms object, continuing to use the previously-
+			* defined $i counter.
+			*/
 			while ($term = $result->fetchRow(MDB2_FETCHMODE_OBJECT))
 			{
 				$terms->$i = $term->term;
@@ -550,7 +630,6 @@ class Dictionary
 		$tmp = array_unique($tmp);
 		$terms = (object) $tmp;
 
-		// Return the result.
 		return $terms;
 	}	
 }
