c3c3283d8bbef053eb065945cb73269366d4f55c
Waldo Jaquith <waldo@jaquith.org>
2013-02-22 11:05:11 -0500

    Update the comment style
    Per #170.

--- includes/page-head.inc.php
+++ includes/page-head.inc.php
@@ -3,6 +3,8 @@
 /**
  * Loads the environment for The State Decoded.
  * 
+ * This is called at the head of every .php file.
+ * 
  * PHP version 5
  *
  * @author		Waldo Jaquith <waldo at jaquith.org>
@@ -14,63 +16,95 @@
  *
  */
 
-# Include the PEAR database abstraction layer. <http://pear.php.net/package/MDB2>
+/* 
+ * Include the PEAR database abstraction layer. <http://pear.php.net/package/MDB2>
+ */
 require_once 'MDB2.php';
 
-# If APC is not running.
+/* 
+ * If APC is not running.
+ */
 if ( !extension_loaded('apc') || (ini_get('apc.enabled') != 1) )
 {
-	# Include the site's config file.
+	/*
+	 * Include the site's config file.
+	 */
 	require_once 'config.inc.php';
 }
 
-# Else if APC is running, get data from the cache.
+/*
+ * Else if APC is running, get data from the cache.
+ */
 else
 {
-	# Attempt to load the config file constants out of APC.
+	/*
+	 * Attempt to load the config file constants out of APC.
+	 */
 	$result = apc_load_constants('config');
 	
-	# If this attempt did not work.
+	/*
+	 * If this attempt did not work.
+	 */
 	if ($result === false)
 	{
-		# Load constants from the config file.
+		/*
+		 * Load constants from the config file.
+		 */
 		require_once 'config.inc.php';
 		
-		# And then save them to APC.
+		/*
+		 * And then save them to APC.
+		 */
 		$constants = get_defined_constants(true);
 		apc_define_constants('config', $constants['user']);
 	}
 }
 
-# Connect to the database.
+/*
+ * Connect to the database.
+ */
 $db =& MDB2::connect(MYSQL_DSN);
 if (PEAR::isError($db))
 {
-	# If a specific error page has been created for database connection failures, display that.
+	/*
+	 * If a specific error page has been created for database connection failures, display that.
+	 */
 	if (defined('ERROR_PAGE_DB'))
 	{
 		require($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.ERROR_PAGE_DB);
 		exit();
 	}
 	
-	# If no special error page exists, display a generic error.
+	/*
+	 * If no special error page exists, display a generic error.
+	 */
 	else
 	{
 		die(SITE_TITLE.' is having some database trouble right now. Please check back in a few minutes.');
 	}
 }
 
-# We must always connect to the database with UTF-8.
+/*
+ * We must always connect to the database with UTF-8.
+ */
 $db->setCharset('utf8');
 
-# Include the functions that drive the site.
+/*
+ * Include the functions that drive the site.
+ */
 require('functions.inc.php');
 
-# Include the custom functions file.
+/*
+ * Include the custom functions file.
+ */
 require(CUSTOM_FUNCTIONS);
 
-# Include WordPress's texturize function, for typographical niceties.
+/*
+ * Include WordPress's texturize function, for typographical niceties.
+ */
 require('texturize.inc.php');
 		
-# We're going to need access to the database connection throughout the site.
+/*
+ * We're going to need access to the database connection throughout the site.
+ */
 global $db;
