d4301f9bd0008c6b607fac4532696df16d904a80
Waldo Jaquith <waldo@jaquith.org>
2013-05-08 11:55:20 -0400

    Provide a link to the state-level regex repository

--- includes/config-sample.inc.php
+++ includes/config-sample.inc.php
@@ -96,8 +96,10 @@ define('GLOBAL_DEFINITIONS', '');
 define('STRUCTURE', 'title,chapter,section');
 
 /*
- * Define the PCRE that identifies section references. It is best to do so without using the section
- * (§) symbol, since section references are frequently made without its presence.
+ * Define the regular expression that identifies section references. It is best to do so without
+ * using a section symbol (e.g., §), since section references are frequently made without its
+ * presence. A growing collection of per-state regular expressions can be found at
+ * <https://github.com/statedecoded/law-identifier>.
  */
 define('SECTION_PCRE', '/([[0-9]{1,})([0-9A-Za-z\-\.]{0,3})-([0-9A-Za-z\-\.:]*)([0-9A-Za-z]{1,})/');
 
