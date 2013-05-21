483554cc33e5add3bfbe520008715a550780026a
Doug Turnbull <dturnbull@o19s.com>
2013-05-21 16:28:04 -0400

    Add some documentation for indexing law xml

--- solrdocs/demos/indexlaws.php
+++ solrdocs/demos/indexlaws.php
@@ -1,16 +1,23 @@
 <?php
 
+
+# The code here indexes each XML document one at a time
+# The update request handler is standard, and not configured
+# specially for state decoded
+#
+# The update request handler in use in the 
 $url = "http://localhost:8983/solr/statedecoded/update";
 require_once('postreq.php');
 require_once('getreq.php');
 
-
+# First we POST to statedecoded/update endpoint and then
+# we commit the changes
 $postFilesReq = new PostFilesRequest($url);
-
 $queryParams = array('tr' => 'stateDecodedXml.xsl');
 
 $postFilesReq->executeGlob($queryParams, 'lawsamples/*.xml');
 
+# This commit makes everything searchable
 $req = new GetRequest($url);
 $req->execute(array('commit' => 'true'));
 
