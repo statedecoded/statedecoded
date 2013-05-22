dbfec96b41c6f21382a670004925a3e4f4f6b859
Doug Turnbull <dturnbull@o19s.com>
2013-05-21 22:28:03 -0400

    Make content type configurable cause its easy enough to do

--- solrdocs/demos/postreq.php
+++ solrdocs/demos/postreq.php
@@ -14,18 +14,20 @@ class PostFilesRequest {
         return $this->urlEndpoint . "?" . httputils\buildQueryString($getParams);
     }
 
-    function executeGlob($getParams, $globPattern) {
+    // Takes a glob pattern and posts 
+    // those files up
+    function executeGlob($getParams, $globPattern, $contentType) {
         $files = array();
         foreach (glob($globPattern) as $filename) {
             $files[] = $filename;
-            $this->execute($getParams, $filename);
+            $this->execute($getParams, $filename, $contentType);
         }
     }
 
 
     // Takes get parameters + an array of file paths 
     // to post. May also specify a single file
-    function execute($getParams, $files) {
+    function execute($getParams, $files, $contentType) {
         // Modified from: 
         // http://stackoverflow.com/a/3892820/8123
 
@@ -40,6 +42,8 @@ class PostFilesRequest {
             $data .= file_get_contents($files[$i]);
         }
 
+        $contentType = array($contentType);
+
         $ch = curl_init();
         $url = $this->fullUrl($getParams);
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -47,7 +51,7 @@ class PostFilesRequest {
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/xml; charset=US-ASCII"));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $contentType); 
         $response = curl_exec($ch);
         return $response;
     }
@@ -58,7 +62,7 @@ if (basename($argv[0]) == basename(__FILE__)) {
     $queryParams = array('tr' => 'stateDecodedXml.xsl');
     $lawSearcher = new PostFilesRequest("http://localhost:8983/solr/statedecoded/update/xslt");
     $file = "lawsamples/31-45.xml";
-    print $lawSearcher->execute($queryParams, $file);
+    print $lawSearcher->execute($queryParams, $file, "Content-Type: application/xml; charset=US-ASCII");
 }
 
 ?>
