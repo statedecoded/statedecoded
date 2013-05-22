edcb799c9f2a6064cae737f2707b08f2ddb89959
Doug Turnbull <dturnbull@o19s.com>
2013-05-21 22:09:44 -0400

    Add documentation on indexing law XML into Solr

--- solrdocs/demos/indexlaws.php
+++ solrdocs/demos/indexlaws.php
@@ -1,23 +1,43 @@
 <?php
-
-
-# The code here indexes each XML document one at a time
-# The update request handler is standard, and not configured
-# specially for state decoded
-#
-# The update request handler in use in the 
-$url = "http://localhost:8983/solr/statedecoded/update";
 require_once('postreq.php');
 require_once('getreq.php');
+
+# ***************************************************************
+# The code here indexes each law XML document one at a time
+#  
+# Solr comes prepackaged with several update request processors
+# that take updates/inserts/deletes to documents. We're using
+# the XSLT update request processor.
+#
+# More on update request processors can be found here:
+#   http://wiki.apache.org/solr/UpdateRequestProcessor
+#
+#
+# How this works is Solr knows that the content is XML from
+# the content type passed in. Solr takes the XML file and 
+# refers to the xslt specified in the tr parameter. Solr then
+# executes the xslt on the passed in xml and the xml should 
+# get translated into Solr Update XML -- the standard Solr XML
+# format for specifying a document.
+#
+$url = "http://localhost:8983/solr/statedecoded/update";
+$contentType = "Content-Type: application/xml; charset=US-ASCII";
 
 # First we POST to statedecoded/update endpoint and then
 # we commit the changes
 $postFilesReq = new PostFilesRequest($url);
 $queryParams = array('tr' => 'stateDecodedXml.xsl');
 
-$postFilesReq->executeGlob($queryParams, 'lawsamples/*.xml');
+$postFilesReq->executeGlob($queryParams, 'lawsamples/*.xml', $contentType);
 
-# This commit makes everything searchable
+# Once files are posted, they are not searchable
+# until a commit is done.
+#
+# For some applications where there's a lot of real time 
+# updates to documents that need to be searched in real time,
+# it may be important to commit often. For state decoded,
+# this is less important as all data is imported at once
+# with a big batch update.
 $req = new GetRequest($url);
 $req->execute(array('commit' => 'true'));
 
