b55c692e5a02253f4a9ba73cc8ae2458fd852389
Waldo Jaquith <waldo@jaquith.org>
2013-06-05 15:26:02 -0400

    Only iterate through structural units if they exist

--- includes/class.State-sample.inc.php
+++ includes/class.State-sample.inc.php
@@ -560,9 +560,12 @@ class Parser
 		// config file as a container for global definitions. If it was, then we override the
 		// presumed scope and provide a global scope.
 		$ancestry = array();
-		foreach ($this->code->structure as $struct)
+		if (isset($this->code->structure))
 		{
-			$ancestry[] = $struct->identifier;
+			foreach ($this->code->structure as $struct)
+			{
+				$ancestry[] = $struct->identifier;
+			}
 		}
 		$ancestry = implode(',', $ancestry);
 		$ancestry_section = $ancestry . ','.$this->code->section_number;
