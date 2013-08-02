e575936b6a9d69e1af7242364881809e3e393b6a
Waldo Jaquith <waldo@jaquith.org>
2013-08-02 17:15:56 -0400

    Suppress cURL output

--- includes/parser-controller.inc.php
+++ includes/parser-controller.inc.php
@@ -917,6 +917,7 @@ class ParserController
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_exec($ch);
 		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
