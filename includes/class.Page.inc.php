51705ed1fd511b058ff7ba8ee20faf775c384994
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-08-13 12:10:40 -0400

    Fixing version number in class.Page.inc.php

--- includes/class.Page.inc.php
+++ includes/class.Page.inc.php
@@ -2,13 +2,13 @@
 
 /**
  * The Page class, for rendering HTML and delivering it to the browser
- * 
+ *
  * PHP version 5
  *
  * @author		Waldo Jaquith <waldo at jaquith.org>
  * @copyright	2010-2013 Waldo Jaquith
  * @license		http://www.gnu.org/licenses/gpl.html GPL 3
- * @version		0.6
+ * @version		0.7
  * @link		http://www.statedecoded.com/
  * @since		0.1
  *
@@ -19,7 +19,7 @@
  */
 class Page
 {
-	
+
 	/**
 	 * A shortcut for all steps necessary to turn variables into an output page.
 	 */
@@ -28,14 +28,14 @@ class Page
 		Page::render();
 		Page::display();
 	}
-	
-	
+
+
 	/**
 	 * Combine the populated variables with the template.
 	 */
 	function render()
 	{
-	
+
 		/*
 		 * Save the contents of the template file to a variable. First check APC and see if it's
 		 * stored there.
@@ -53,7 +53,7 @@ class Page
 		{
 			$this->html = file_get_contents(INCLUDE_PATH . '/templates/' . TEMPLATE . '.inc.php');
 		}
-		
+
 		/*
 		 * Create the browser title.
 		 */
@@ -73,18 +73,18 @@ class Page
 		{
 			$this->field->browser_title .= '—' . SITE_TITLE;
 		}
-		
+
 		/*
 		 * Include the place name (e.g., "Washington," "Texas," "United States").
 		 */
 		$this->field->place_name = PLACE_NAME;
-		
+
 		/*
 		 * If a Google Analytics Web Property ID has been provided, insert the tracking code.
 		 */
 		if (defined('GOOGLE_ANALYTICS_ID'))
 		{
-		
+
 			$this->field->google_analytics =
 				  "var _gaq = _gaq || [];
 				  _gaq.push(['_setAccount', '" . GOOGLE_ANALYTICS_ID . "']);
@@ -94,10 +94,10 @@ class Page
 					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 				  })();";
-				  
+
 		}
-		
-		
+
+
 		/*
 		 * Replace all of our in-page tokens with our defined variables.
 		 */
@@ -105,12 +105,12 @@ class Page
 		{
 			$this->html = str_replace('{{' . $field . '}}', $contents, $this->html);
 		}
-		
+
 		/*
 		 * Erase any unpopulated tokens that remain in our template.
 		 */
 		$this->html = preg_replace('/{{[0-9a-z_]+}}/', '', $this->html);
-		
+
 		/*
 		 * Erase selected containers, if they're empty.
 		 */
@@ -118,20 +118,20 @@ class Page
 		$this->html = preg_replace('/<nav id="intercode">(\s*)<\/nav>/', '', $this->html);
 		$this->html = preg_replace('/<nav id="breadcrumbs">(\s*)<\/nav>/', '', $this->html);
 	}
-	
+
 	/**
 	 * Send the page to the browser.
 	 */
 	function display()
 	{
-	
+
 		if (!isset($this->html))
 		{
 			return FALSE;
 		}
-		
+
 		echo $this->html;
 		return TRUE;
-		
+
 	}
 }
