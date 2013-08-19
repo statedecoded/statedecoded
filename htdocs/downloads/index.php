daa6aede8ca923cafbc52dee5c2dc67970a32572
Waldo Jaquith <waldo@jaquith.org>
2013-08-19 15:26:43 -0400

    Update comment style

--- htdocs/downloads/index.php
+++ htdocs/downloads/index.php
@@ -14,13 +14,19 @@
  *
  */
 
-# Include the PHP declarations that drive this page.
+/*
+ * Include the PHP declarations that drive this page.
+ */
 require $_SERVER['DOCUMENT_ROOT'] . '/../includes/page-head.inc.php';
 
-# Fire up our templating engine.
+/*
+ * Fire up our templating engine.
+ */
 $template = new Page;
 
-# Define some page elements.
+/*
+ * Define some page elements.
+ */
 $template->field->browser_title = 'Downloads';
 $template->field->page_title = 'Downloads';
 
@@ -45,14 +51,20 @@ $body = '
 
 $sidebar = '';
 
-# Put the shorthand $body variable into its proper place.
+/*
+ * Put the shorthand $body variable into its proper place.
+ */
 $template->field->body = $body;
 unset($body);
 
-# Put the shorthand $sidebar variable into its proper place.
+/*
+ * Put the shorthand $sidebar variable into its proper place.
+ */
 $template->field->sidebar = $sidebar;
 unset($sidebar);
 
-# Parse the template, which is a shortcut for a few steps that culminate in sending the content
-# to the browser.
+/*
+ * Parse the template, which is a shortcut for a few steps that culminate in sending the content
+ * to the browser.
+ */
 $template->parse();
