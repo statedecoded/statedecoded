355273d3beaacc403f83e52af84b335aa9b4341f
Waldo Jaquith <waldo@jaquith.org>
2013-08-21 16:44:08 -0400

    Standardize the coding style

--- includes/class.Router.inc.php
+++ includes/class.Router.inc.php
@@ -17,25 +17,34 @@
 
 class Router
 {
-	// $routes keeps the order
+
+	/*
+	 * $routes keeps the order
+	 */
 	public static $routes = array();
 
-	// $handlers is our lookup table
+	/*
+	 * $handlers is our lookup table
+	 */
 	public static $handlers = array();
 
 	public static function addRoute($route, $handler)
 	{
+	
 		if (self::$handlers[$route])
 		{
 			trigger_error(
-				'The route "'.$route.'" already exists and will be replaced.',
+				'The route "' . $route . '" already exists and will be replaced.',
 				E_USER_WARNING
 			);
 		}
 
-		// The default is always last, so use index -1 and length 0.
+		/*
+		 * The default is always last, so use index -1 and length 0.
+		 */
 		array_splice(self::$routes, -1, 0, $route);
 		self::$handlers[$route] = $handler;
+		
 	}
 
 	public static function getRoutes()
@@ -45,17 +54,25 @@ class Router
 
 	public static function getRoute($url)
 	{
+	
 		foreach(self::$routes as $route) {
-			// Escape slashes
+			
+			/*
+			 * Escape slashes.
+			 */
 			$route_regex = str_replace('/', '\\/', $route);
 
-			if (preg_match('/'.$route_regex.'/', $url, $matches))
+			if (preg_match('/' . $route_regex . '/', $url, $matches))
 			{
 				return array(self::$handlers[$route], $matches);
 			}
+			
 		}
 
-		// The last route is the default
+		/*
+		 * The last route is the default.
+		 */
 		return array(self::$handlers[end(self::$routes)], array());
+		
 	}
 }
