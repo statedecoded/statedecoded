c2fd7240a571758a9dc7b63b6c80a364bbc36836
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-08-23 17:09:38 -0400

    Allowing versionless api calls.  #387

--- includes/routes.inc.php
+++ includes/routes.inc.php
@@ -34,12 +34,28 @@ Router::addRoute('^/api-key/$', array('ApiKeyController', 'requestKey'));
  */
 
 // API
+
+// Latest version
+Router::addRoute('^/api/structure/(?P<identifier>([0-9A-Za-z\.]{1,8}/)*([0-9A-Za-z\.]{1,8}))/',
+	'api/1.0/structure.php');
+
+// Other version
 Router::addRoute('^/api/(?P<api_version>1.0)/structure/(?P<identifier>([0-9A-Za-z\.]{1,8}/)*([0-9A-Za-z\.]{1,8}))/',
 	'api/1.0/structure.php');
 
+// Latest version
+Router::addRoute('^/api/law/(?P<section>[0-9A-Za-z\.]{1,4}-[0-9\.:]{1,10})/',
+	'api/1.0/law.php');
+
+// Other version
 Router::addRoute('^/api/(?P<api_version>1.0)/law/(?P<section>[0-9A-Za-z\.]{1,4}-[0-9\.:]{1,10})/',
 	'api/1.0/law.php');
 
+// Latest version
+Router::addRoute('^/api/dictionary/(?P<term>.*)',
+	'api/1.0/dictionary.php');
+
+// Other version
 Router::addRoute('^/api/(?P<api_version>1.0)/dictionary/(?P<term>.*)',
 	'api/1.0/dictionary.php');
 
