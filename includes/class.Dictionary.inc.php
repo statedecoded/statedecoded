c9846837ffae455817fb9c2e2e86a52347159901
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-08-23 11:35:16 -0400

    Replacing all queries with prepared statements in Dictionary class. #278

--- includes/class.Dictionary.inc.php
+++ includes/class.Dictionary.inc.php
@@ -2,7 +2,7 @@
 
 /**
  * The Dictionary class, for retrieving terms and definitions
- * 
+ *
  * PHP version 5
  *
  * @author		Waldo Jaquith <waldo at jaquith.org>
@@ -15,22 +15,22 @@
  */
 
 /**
- * 
+ *
  */
 class Dictionary
 {
-	
+
 	/**
 	 * Get the definition for a given term for a given section of code.
 	 */
 	function define_term()
 	{
-		
+
 		/*
 		 * We're going to need access to the database connection throughout this class.
 		 */
 		global $db;
-		
+
 		/*
 		 * If no term has been defined, there is nothing to be done.
 		 */
@@ -38,7 +38,7 @@ class Dictionary
 		{
 			return FALSE;
 		}
-		
+
 		/*
 		 * Determine the structural heritage of the provided section number and store it in an
 		 * array.
@@ -55,7 +55,7 @@ class Dictionary
 				$ancestry[] = $tmp->id;
 			}
 		}
-		
+
 		/*
 		 * We want to check if the term is in all caps. If it is, then we want to keep it in
 		 * all caps to query the database. Otherwise, we lowercase it. That is, "Board" should be looked
@@ -73,21 +73,22 @@ class Dictionary
 				break;
 			}
 		}
-		
+
 		if ($lowercase === TRUE)
 		{
 			$this->term = strtolower($this->term);
 		}
-		
+
 		/*
 		 * If the last character in this word is an "s," then it might be a plural, in which
 		 * case we need to search for this and without its plural version.
 		 */
+
 		if (substr($this->term, -1) == 's')
 		{
 			$plural = TRUE;
 		}
-		
+
 		/*
 		 * This is a tortured assembly of a query. The idea is to provide flexibility on a pair of
 		 * axes. The first is to support both plural and singular terms. The second is to support
@@ -99,46 +100,55 @@ class Dictionary
 				FROM dictionary
 				LEFT JOIN laws
 					ON dictionary.law_id=laws.id
-				WHERE (dictionary.term=' . $db->quote($this->term);
+				WHERE (dictionary.term = :term';
+		$sql_args = array(
+			':term' => $this->term
+		);
 		if ($plural === TRUE)
 		{
-			$sql .= ' OR dictionary.term = ' . $db->quote(substr($this->term, 0, -1));
+			$sql .= ' OR dictionary.term = :term_single';
+			$sql_args[':term_single'] =  substr($this->term, 0, -1);
 		}
 		$sql .= ') ';
 		if (isset($this->section_number))
 		{
 			$sql .= 'AND (';
-			foreach ($ancestry as $structure_id)
+
+			$ancester_count = count($ancestry);
+			for ($i = 0; $i < $ancester_count; $i++)
 			{
-				$sql .= '(dictionary.structure_id = ' . $db->quote($structure_id) . ') OR';
+				$sql .= "(dictionary.structure_id = :structure_id$i) OR ";
+				$sql_args[":structure_id$i"] = $ancestry[$i];
 			}
-			$sql .= '	(dictionary.scope = "global")
+			$sql .= '	(dictionary.scope = :scope)
 					OR
-						(laws.section = ' . $db->quote($this->section_number) . ')
+						(laws.section = :section_number)
 					) ';
+			$sql_args['scope'] = 'global';
+			$sql_args[':section_number'] = $this->section_number;
 		}
-		
+
 		$sql .= 'ORDER BY dictionary.scope_specificity ';
 		if (isset($this->section_number))
 		{
-		
+
 			$sql .= 'LIMIT 1';
 		}
-		
-		$result = $db->query($sql);
 
+		$statement = $db->prepare($sql);
+		$result = $statement->execute($sql_args);
 		/*
 		 * If the query succeeds, great, retrieve it.
 		 */
-		if ( ($result !== FALSE) && ($result->rowCount() > 0) )
+		if ( ($result !== FALSE) && ($statement->rowCount() > 0) )
 		{
-		
+
 			/*
 			 * Get all results.
 			 */
 			$dictionary = new stdClass();
 			$i=0;
-			while ($term = $result->fetch(PDO::FETCH_OBJ))
+			while ($term = $statement->fetch(PDO::FETCH_OBJ))
 			{
 				$term->url = 'http://' . $_SERVER['SERVER_NAME'] . '/' . $term->section_number . '/';
 				$term->formatted = wptexturize($term->definition) . ' (<a href="' . $term->url . '">'
@@ -147,51 +157,56 @@ class Dictionary
 				$i++;
 			}
 		}
-		
+
 		/*
 		 * Else if the query fails, then the term is found in the generic terms dictionary.
 		 */
 		else
 		{
-		
+
 			/*
 			 * Assemble the SQL.
 			 */
 			$sql = 'SELECT term, definition, source, source_url AS url
 					FROM dictionary_general
-					WHERE term=' . $db->quote($this->term);
+					WHERE term = :term';
+			$sql_args = array(
+				':term' => $this->term
+			);
 			if ($plural === TRUE)
 			{
-				$sql .= ' OR term = ' . $db->quote(substr($this->term, 0, -1));
+				$sql .= ' OR term = :term_single';
+				$sql_args[':term_single'] = substr($this->term, 0, -1);
 			}
 			$sql .= ' LIMIT 1';
-			
-			$result = $db->query($sql);
-			
+
+			$statement = $db->prepare($sql);
+			$result = $statement->execute($sql_args);
+
 			/*
 			 * If the query fails, or if no results are found, return false -- we have no terms for
 			 * this structural unit.
 			 */
-			if ( ($result === FALSE) || ($result->rowCount() < 1) )
+			if ( ($result === FALSE) || ($statement->rowCount() < 1) )
 			{
 				return FALSE;
 			}
-		
+
 			/*
 			 * Get the first result. Assemble a slightly different response than for a custom term.
 			 * We assign this to the first element of an object because that is the format that the
 			 * API expects to receive a list of terms in. In this case, we have just one term.
 			 */
-			$dictionary->{0} = $result->fetch(PDO::FETCH_OBJ);
+			$dictionary->{0} = $statement->fetch(PDO::FETCH_OBJ);
 			$dictionary->{0}->formatted = wptexturize($dictionary->{0}->definition) .
 				' (<a href="' . $dictionary->{0}->url . '">' . $dictionary->{0}->source . '</a>)';
 
 		}
-		
+
 		return $dictionary;
-		
+
 	}
-		
+
 	/**
 	 * Get a list of defined terms for a given structural unit of the code, returning just a listing
 	 * of terms. (The idea is that we can use an Ajax call to get each definition on demand.)
@@ -203,7 +218,7 @@ class Dictionary
 		 * We're going to need access to the database connection throughout this class.
 		 */
 		global $db;
-		
+
 		/*
 		 * If a structural ID hasn't been passed to this function, then return a listing of terms
 		 * that apply to the entirety of the code.
@@ -212,7 +227,7 @@ class Dictionary
 		{
 			$this->scope = 'global';
 		}
-		
+
 		/*
 		 * Get a listing of all structural units that contain the current structural unit -- that is,
 		 * if this is a chapter, get the ID of both the chapter and the title. And so on.
@@ -234,15 +249,18 @@ class Dictionary
 		/*
 		 * Get a listing of all globally scoped terms.
 		 */
+		$sql_args = array(
+			':global_scope' => 'global'
+		);
 		if ( isset($this->scope) && ($this->scope == 'global') )
 		{
 			$sql = 'SELECT dictionary.term
 					FROM dictionary
 					LEFT JOIN laws
 						ON dictionary.law_id=laws.id
-					 WHERE scope="global"';
+					 WHERE scope = :global_scope';
 		}
-		
+
 		/*
 		 * Otherwise, we're getting a listing of all more narrowly scoped terms. We always make sure
 		 * that global definitions are included, in addition to the definitions for the current
@@ -258,33 +276,38 @@ class Dictionary
 						ON laws.structure_id=structure.id
 					WHERE
 					(
-						(dictionary.law_id=' . $db->quote($this->section_id) . ')
+						(dictionary.law_id=:section_id)
 						AND
-						(dictionary.scope="section")
+						(dictionary.scope=:section_scope)
 					)';
-			foreach ($ancestry as $structure_id)
+			$sql_args[':section_id'] = $this->section_id;
+			$sql_args[':section_scope'] = 'section';
+			$ancestry_count = count($ancestry);
+			for($i = 0; $i < $ancestry_count; $i++)
 			{
-				$sql .= ' OR (dictionary.structure_id=' . $db->quote($structure_id) . ')';
+				$sql .= " OR (dictionary.structure_id=:structure$i)";
+				$sql_args[":structure$i"] = $ancestry[$i];
 			}
-			$sql .= ' OR (scope="global")';
+			$sql .= ' OR (scope=:global_scope)';
 		}
 
-		$result = $db->query($sql);
-		
+		$statement = $db->prepare($sql);
+		$result = $statement->execute($sql_args);
+
 		/*
 		 * If the query fails, or if no results are found, return false -- we have no terms for this
 		 * structural unit.
 		 */
-		if ( ($result === FALSE) || ($result->rowCount() < 1) )
+		if ( ($result === FALSE) || ($statement->rowCount() < 1) )
 		{
 			return FALSE;
 		}
-		
+
 		/*
 		 * Build up the result as an object as we loop through the results.
 		 */
 		$i=0;
-		while ($term = $result->fetch(PDO::FETCH_OBJ))
+		while ($term = $statement->fetch(PDO::FETCH_OBJ))
 		{
 			$terms->$i = $term->term;
 			$i++;
@@ -295,26 +318,28 @@ class Dictionary
 		 */
 		$sql = 'SELECT term
 				FROM dictionary_general';
+		$sql_args = null;
 
-		$result = $db->query($sql);
-		
-		if ($result->rowCount() > 0)
-		{		
+		$statement = $db->prepare($sql);
+		$result = $statement->execute($sql_args);
+
+		if ($result !== FALSE && $statement->rowCount() > 0)
+		{
 			/*
 			* Append these results to the existing $terms object, continuing to use the previously-
 			* defined $i counter.
 			*/
-			while ($term = $result->fetch(PDO::FETCH_OBJ))
+			while ($term = $statement->fetch(PDO::FETCH_OBJ))
 			{
 				$terms->$i = $term->term;
 				$i++;
 			}
 		}
-		
+
 		$tmp = (array) $terms;
 		$tmp = array_unique($tmp);
 		$terms = (object) $tmp;
 
 		return $terms;
-	}	
+	}
 }
