06f1b19be92244bcabfa6c723051d30813cad039
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-08-29 11:57:22 -0400

    Splitting logger classes into separate files.

--- includes/class.Logger.inc.php
+++ includes/class.Logger.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * Generic Logger classes
+ * Base Logger class
  *
- * Classes to output messages in a given format.
+ * Class to output messages in a given format. Can be HTML or plain text.
  *
  * PHP version 5
  *
@@ -14,10 +14,6 @@
  * @link		http://www.statedecoded.com/
  * @since		0.7
 */
-
-/**
- * Base Logger - just outputs a message.  Can be HTML or plain text.
- */
 
 class Logger
 {
@@ -92,97 +88,4 @@ class Logger
 
 	// }}}
 
-}
-
-
-/**
- * Silent Logger - does nothing.
- */
-
-class QuietLogger
-{
-	public function message($msg) {}
-}
-
-
-/**
- * Debug Logger - includes time and memory usage.
- */
-
-class DebugLogger extends Logger
-{
-   /**
-     * Time the logger was first created.
-     *
-     * You shouldn't ever mess with this (probably).
-     * It would be nice if we could get the time the script started
-     * but this will do for now.
-     *
-     * @var integer
-     */
-	public $start_time;
-
-	// {{{ __construct
-
-	/**
-	 * Class constructor.  Sets defaults.
-	 *
- 	 * We need to set the time the logger was created.
- 	 *
-	 * @param array    Hash of all values to override in the class
-	 */
-
-	public function __construct($args)
-	{
-		parent::__construct($args);
-
-		$this->start_time = $this->get_time();
-	}
-
-	// {{{ message
-
-	/**
-	 * Prints the message, adds the time elapsed and memory usage.
-	 */
-	public function message($msg, $level)
-	{
-		echo $this->get_time_elapsed() . "ms ";
-		echo memory_get_usage() . "b : ";
-
-		parent::message($msg, $level);
-	}
-
-	// }}}
-
-	// {{{ get_time
-
-	/**
-	 * Gets the current (micro) time.  Nothing fancy.
-	 */
-	public function get_time()
-	{
-		return microtime(TRUE);
-	}
-
-	// }}}
-
-	// {{{ get_time_elapsed
-
-	/**
-	 * Determines how much time has passed since the Logger was started.
-	 *
-	 * @param integer $level The log level of the message.
-	 *                      This log level must be greater than the log level
-	 *                      set on the class to actually be printed.
-	 */
-	public function get_time_elapsed($time)
-	{
-		if(!$time)
-		{
-			$time = $this->get_time();
-		}
-		return $time - $this->start_time;
-	}
-
-	// }}}
 }
