fbf718937b386f410abd60806da4ce41d50c000f
Waldo Jaquith <waldo@jaquith.org>
2013-10-07 11:42:27 -0400

    Update code style to meet present project standards

--- htdocs/api/1.0/structure.php
+++ htdocs/api/1.0/structure.php
@@ -20,17 +20,23 @@ header('Content-type: application/json');
 $api = new API;
 $api->list_all_keys();
 
-# Make sure that the key is the correct (safe) length.
+/*
+ * Make sure that the key is the correct (safe) length.
+ */
 if ( strlen($_GET['key']) != 16 )
 {
 	json_error('Invalid API key.');
 	die();
 }
 
-# Localize the key, filtering out unsafe characters.
+/*
+ * Localize the key, filtering out unsafe characters.
+ */
 $key = filter_input(INPUT_GET, 'key', FILTER_SANITIZE_STRING);
 
-# If no key has been passed with this query, or if there are no registered API keys.
+/*
+ * If no key has been passed with this query, or if there are no registered API keys.
+ */
 if ( empty($key) || (count($api->all_keys) == 0) )
 {
 	json_error('API key not provided. Please register for an API key.');
@@ -42,12 +48,16 @@ elseif (!isset($api->all_keys->$key))
 	die();
 }
 
-# Use a provided JSONP callback, if it's safe.
+/*
+ * Use a provided JSONP callback, if it's safe.
+ */
 if (isset($_REQUEST['callback']))
 {
 	$callback = $_REQUEST['callback'];
 
-	# If this callback contains any reserved terms that raise XSS concerns, refuse to proceed.
+	/*
+	 * If this callback contains any reserved terms that raise XSS concerns, refuse to proceed.
+	 */
 	if (valid_jsonp_callback($callback) === false)
 	{
 		json_error('The provided JSONP callback uses a reserved word.');
@@ -55,22 +65,27 @@ if (isset($_REQUEST['callback']))
 	}
 }
 
-# If no identifier has been specified, explicitly make it a null variable. This is when the request
-# is for the top level -- that is, a listing of the fundamental units of the code (e.g., titles).
+/*
+ * If no identifier has been specified, explicitly make it a null variable. This is when the request
+ * is for the top level -- that is, a listing of the fundamental units of the code (e.g., titles).
+ */
 if ( !isset($args['identifier']) || empty($args['identifier']) )
 {
 	$identifier = '';
 }
 
-# If an identifier has been specified (which may come in the form of multiple identifiers, separated
-# by slashes), localize that variable.
+/*
+ * If an identifier has been specified (which may come in the form of multiple identifiers,
+ * separated by slashes), localize that variable.
+ */
 else
 {
-	# Localize the identifier, filtering out unsafe characters.
+	/*
+	 * Localize the identifier, filtering out unsafe characters.
+	 */
 	$identifier = filter_var($args['identifier'], FILTER_SANITIZE_STRING);
 }
 
-# Create a new instance of the class that handles information about individual laws.
 /*
  * If the request is for the structural units sorted by a specific criteria.
  */
@@ -87,44 +102,61 @@ if (isset($args['sort']))
 	
 }
 
+/*
+ * Create a new instance of the class that handles information about individual laws.
+ */
 $struct = new Structure();
 
 
-# Get the structure based on our identifier
+/*
+ * Get the structure based on our identifier.
+ */
 $struct->token_to_structure($identifier);
 $response = $struct->structure;
 
-# If this structural element does not exist.
-if ($response === false)
+/*
+ * If this structural element does not exist.
+ */
+if ($response === FALSE)
 {
 	json_error('Section not found.');
 	die();
 }
 
-# List all child structural units.
 /*
  * List all child structural units.
  */
 $struct->order_by = $order_by;
 $response->children = $struct->list_children();
 
-# List all laws.
+/*
+ * List all laws.
+ */
 $response->laws = $struct->list_laws();
 
-# If the request contains a specific list of fields to be returned.
+/*
+ * If the request contains a specific list of fields to be returned.
+ */
 if (isset($_GET['fields']))
 {
-	# Turn that list into an array.
+
+	/*
+	 * Turn that list into an array.
+	 */
 	$returned_fields = explode(',', urldecode($_GET['fields']));
 	foreach ($returned_fields as &$field)
 	{
 		$field = trim($field);
 	}
 
-	# It's essential to unset $field at the conclusion of the prior loop.
+	/*
+	 * It's essential to unset $field at the conclusion of the prior loop.
+	 */
 	unset($field);
 
-	# Step through our response fields and eliminate those that aren't in the requested list.
+	/*
+	 * Step through our response fields and eliminate those that aren't in the requested list.
+	 */
 	foreach($response as $field => &$value)
 	{
 		if (in_array($field, $returned_fields) === false)
@@ -132,16 +164,23 @@ if (isset($_GET['fields']))
 			unset($response->$field);
 		}
 	}
+	
 }
 
-# Include the API version in this response.
-if(isset($args['api_version']) && strlen($args['api_version'])) {
+/*
+ * Include the API version in this response.
+ */
+if (isset($args['api_version']) && strlen($args['api_version']))
+{
 	$response->api_version = filter_var($args['api_version'], FILTER_SANITIZE_STRING);
 }
 else {
 	$response->api_version = CURRENT_API_VERSION;
 }
 
+/*
+ * Optionally wrap our response in a callback, and flush all data to the client.
+ */
 if (isset($callback))
 {
 	echo $callback.' (';
