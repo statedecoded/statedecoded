d3a967868e2764f0ee5566a7a62800f287888efd
Bill Hunt <bill@krues8dr.com>
2014-02-14 17:12:41 -0500

    Creating Task runner.  #414

---
+++ includes/task/class.CliAction.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class CliAction
+{
+	static public $name;
+
+	abstract public function execute($args = array());
+
+	/*
+	 * Technically, a static abstract function doesn't make sense,
+	 * since static methods belong to the class that defined them
+	 * and cannot be overridden - but you really must have this
+	 * function for these classes to work.
+	 */
+	//abstract public static function getHelp($args = array()) {}
+}
