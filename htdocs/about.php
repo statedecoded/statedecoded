84c87110e909e570ea79779d19eb73fb55dca948
Bill Hunt <bill@krues8dr.com>
2017-01-14 21:15:17 -0500

    Creating about page with content from the main StateDecoded site. Fixes #291

--- htdocs/about.php
+++ htdocs/about.php
@@ -23,7 +23,12 @@ $content = new Content();
 $content->set('browser_title', 'About');
 $content->set('page_title', 'About');
 
-$body = '';
+$body = '
+<p>Legal codes are wretched. Seriously, look at a few: <a href="http://www.leginfo.ca.gov/calaw.html">California’s</a>, <a href="http://public.leginfo.state.ny.us/menugetf.cgi?COMMONQUERY=LAWS">New York’s</a>, <a href="http://www.ilga.gov/legislation/ilcs/ilcs.asp">Illinois’</a>, and <a href="http://www.statutes.legis.state.tx.us/">Texas’</a> are all good examples of how stunningly difficult that it is to understand legal codes. They don’t have APIs. Virtually none have bulk downloads. You’re stuck with their crude offerings.</p>
+<p>The State Decoded is a platform that displays legal codes, court decisions, and information from legislative tracking services to make it all more understandable to normal humans. With beautiful typography, embedded definitions of legal terms, and a robust API, this project aims to make our laws a centerpiece of media coverage.</p>
+<p>In development since June 2010, The State Decoded is in open beta testing now, with <a href="https://www.statedecoded.com/places/">a growing network of sites running the software</a>. This work is made possible by <a href="http://www.knightfoundation.org/grants/20110158/">a generous grant from the Knight Foundation</a>.</p>
+<p><a href="https://github.com/waldoj/statedecoded">The LAMP-based code is GPLd and on Github</a>. The goal is to get an organization in every state in the union (and D.C., Guam, and Puerto Rico!) to install this software and create a website for their own state’s code. Interested in stepping up to the plate for your fellow citizens? <a href="https://www.statedecoded.com/contact/">E-mail us!</a></p>
+';
 
 $sidebar = '';
 
@@ -42,7 +47,8 @@ unset($sidebar);
 /*
  * Add the custom classes to the body.
  */
-$content->set('body_class', 'law inside');
+$content->set('body_class', 'inside');
+$content->set('content_class', 'nest narrow');
 
 
 /*
