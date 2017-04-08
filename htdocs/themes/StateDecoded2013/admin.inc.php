9e0d556952e324cd7c9cca066fc81fb447463b25
Bill Hunt <bill@krues8dr.com>
2017-04-08 12:59:37 -0400

    Finally fix the admin header.

--- htdocs/themes/StateDecoded2013/admin.inc.php
+++ htdocs/themes/StateDecoded2013/admin.inc.php
@@ -28,16 +28,23 @@
 	<header id="page_header">
 		<div class="nest">
 			<a href="/" class="noprint">
-				<hgroup id="place_logo">
+				<div id="place_logo">
 					<h1>{{place_name}}</h1>
 					<h2>Decoded</h2>
-				</hgroup>
+				</div>
 			</a>
-			<section id="search">
+			<section id="search-form" class="search">
 				<form id="search_form" method="get" action="/search/">
-					<label for="search">Search the code by keyword, phrase, or title</label>
-					<input type="search" name="q" value="" id="search" placeholder="Search the Laws">
-					<input type="submit" name="" value="Search" id="submit" class="btn btn-success">
+					<div class="form_field text_field_container">
+						<label for="search">Search the code by keyword, phrase, or title</label>
+						<input type="search" name="q" value="{{search_terms}}" id="search" placeholder="Search the Laws">
+					</div>
+					<div class="form_field">
+						{{edition_select}}
+					</div>
+					<div class="form_field">
+						<input type="submit" name="" value="Search" id="submit" class="btn btn-success">
+					</div>
 					<!--a class="advanced" href="#">Advanced</a-->
 				</form>
 			</section> <!-- // #search -->
